7ee1df36fbd791ddfe2eaa77ba134423346481da
Jake Rowsell <jakerowsell@bgl-mac-009556.bgl.financial.local>
2019-12-04 20:14:15 +0000

    ✨ Day 4

---
+++ days/4/part1.js
@@ -0,0 +1,21 @@
+const MIN = 273025
+const MAX = 767253
+
+const hasMatchingAdjacentDigits = str =>
+  str
+    .split("")
+    .some((n, i, arr) => arr[i - 1] === n)
+
+const digitsAscend = str =>
+  str.split("").sort().join('') === str
+
+let matches = 0
+
+for (let i = MIN; i <= MAX; i++) {
+  const str = String(i)
+  if (hasMatchingAdjacentDigits(str) && digitsAscend(str)) {
+    matches++
+  }
+}
+
+console.log(matches)
